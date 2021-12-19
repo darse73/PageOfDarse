538d8fee9f51c515065646f42cc8ac5bdd30bbe0
darse73 <k.yu.713558.2016113@gmail.com>
2021-12-20 00:48:12 +0900

    かざとに教えてもらったが、firestorageが使えない

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";  //fairebaseの初期化
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBI-CoGeYXJMlCEiMWOjBLQmTzj7n7-Tvo",
+    authDomain: "pageofdarse.firebaseapp.com",
+    projectId: "pageofdarse",
+    storageBucket: "pageofdarse.appspot.com",
+    messagingSenderId: "360556229496",
+    appId: "1:360556229496:web:9d95860a69fc7c5ae3ce8d",
+    measurementId: "G-NS2WCBJD5L"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+export const db = getFirestore();
+export const storage = firebase.storage()
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,3 +1,67 @@
 <template>
-    <div class="upload"></div>
+  <div class="upload">
+    <div class="title">
+      <img class="title-img" src="../assets/jerryfish.png" />
+      <h2 class="title-text">最近更新されたページ</h2>
+    </div>
+    <div class="form">
+        <div class="form-title">
+        <h3>タイトル</h3>
+        <input type="text"></div>
+        <div class="img">
+            <h3>プレビュー画像または動画</h3>
+            <input type="file" id="file" name="img" accept="image/*" >
+        </div>
+        <div class="comment">
+          <h3>コメント</h3>
+          <textarea name="comment" cols="17" rows="7"></textarea>
+        </div>
+    <button v-on:click="tweet">送信</button>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.title {
+  display: flex;
+  height: 5rem;
+}
+.title-img {
+  width: 5rem;
+}
+.title-text {
+  font-size: 2.5rem;
+  line-height: 5rem;
+}
+</style>
+
+<script>
+import { collection, addDoc } from "firebase/firestore"
+import { db } from "@/firebase.js"
+import { storage } from "@/firebase.js"
+
+export default {
+  data() {
+    return {
+      inputTweet: "asdfasdfa",
+    }
+  },
+  methods: {
+    async tweet() {
+      const docRef = await addDoc(collection(db, "messages"), {
+        comments: "assf", //this.inputTweet
+      })
+      console.log(docRef)
+
+      const file = document.getElementById("file").files[0]
+      const storageRef = storage.ref()
+      const metadata = {
+        contentType: "image/*"
+      }
+
+      const uploadTask = storageRef.child(`image/${file.name}`).put(file, metadata)
+  console.log(uploadTask)
+    },
+  },
+}
+</script>
