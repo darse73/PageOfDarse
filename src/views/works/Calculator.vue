a8b111839fdd6e7cf947b1dfbd602e148f5abbd1
darse73 <k.yu.713558.2016113@gmail.com>
2021-12-26 17:35:27 +0900

    ビルド、デプロイ完了

--- src/views/works/Calculator.vue
+++ src/views/works/Calculator.vue
@@ -13,7 +13,7 @@
         正直仕組みをよくわかっていないところもあるので、そこは分かり次第MEMORUNDOMの方に載せていこうと思います！
       </p>
 
-      <a href="works-holder/lesson_works/calculator/index.html"
+      <a href="works-holder/calculator/index.html"
         ><img src="../../assets/calcurator.png"
       /></a>
       <p id="go-the-work">画像を押すと実際の作品ページに行きます</p>
