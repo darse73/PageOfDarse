e6c414e7636b14c94914df0dfb799d3b103c07be
darse73 <k.yu.713558.2016113@gmail.com>
2022-01-10 23:18:50 +0900

    BigTitleのレスポンシブ完成

--- src/components/BigTitle.vue
+++ src/components/BigTitle.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="big-title">
     <div class="title-text">
-      <h6><span>くらげ</span>のダースが書く</h6>
-      <h1>プログラミング備忘録</h1>
+      <h6><span class="span-jer">くらげ</span>のダースが書く</h6>
+      <h1><span class="span-tex">プログラミング</span><span class="span-tex">備忘録</span></h1>
     </div>
   </div>
 </template>
@@ -19,7 +19,8 @@
   background-color: rgba(228, 243, 255, 0.877);
   border-bottom: 1rem solid rgb(95, 95, 255);
 }
-.title-text {
+@media screen and (min-width: 1025px){
+  .title-text {
   text-align: center;
   width: 100%;
   position: absolute;
@@ -27,15 +28,59 @@
   left: 50%;
   transform: translate(-50%, -50%);
 }
-span {
+.span-jer {
   color: rgba(0, 89, 255, 0.699);
   font-size: 6rem;
 }
 .title-text h6 {
-  font-size: 4rem;
+  font-size: 4.5rem;
   margin-bottom: 8rem;
 }
-.title-text h1 {
+.span-tex {
   font-size: 10rem;
+}
+}
+@media screen and (max-width: 1024px){
+  .title-text {
+  text-align: center;
+  width: 100%;
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.span-jer {
+  color: rgba(0, 89, 255, 0.699);
+  font-size: 6rem;
+}
+.title-text h6 {
+  font-size: 4.5rem;
+  margin-bottom: 3rem;
+}
+.span-tex {
+  font-size: 8.5rem;
+  display: inline-block;
+}
+}
+@media screen and (max-width: 599px){
+  .title-text {
+  text-align: center;
+  width: 100%;
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.span-jer {
+  color: rgba(0, 89, 255, 0.699);
+  font-size: 3.5rem;
+}
+.title-text h6 {
+  font-size: 2.5rem;
+  margin-bottom: 3rem;
+}
+.span-tex {
+  font-size: 4.5rem;
+}
 }
 </style>
